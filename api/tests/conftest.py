08b8565022f1b734e36820a3ea55fba88b5f4aae
Giovanni Armane <giovanniarmane@gmail.com>
2020-06-11 01:28:36 -0300

    test(tests.conftest): configure max attempts for development mode for Hypothesis

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,3 +1,4 @@
+from hypothesis import Verbosity
 from hypothesis import settings as hypo_settings
 
 from iheroes_api.config.environment import get_settings
@@ -5,6 +6,10 @@ from iheroes_api.config.environment import get_settings
 settings = get_settings()
 
 
-# Configure Hypothesis
-hypo_settings.register_profile("development", max_examples=10)
-hypo_settings.get_profile(settings.HYPOTHESIS_PROFILE)
+# Configure Hypothesis profiles
+hypo_settings.register_profile(
+    "development", max_examples=10, verbosity=Verbosity.verbose
+)
+hypo_settings.register_profile("ci", max_examples=1000)
+
+hypo_settings.load_profile(settings.HYPOTHESIS_PROFILE)
