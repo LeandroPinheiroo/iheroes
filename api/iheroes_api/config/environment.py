09aa352730fbd4c5a023cc852d2681f51d999892
Giovanni Armane <giovanniarmane@gmail.com>
2020-07-26 20:37:38 -0300

    feat(iheroes_api.config.environment): add env var for listener and type environments

--- api/iheroes_api/config/environment.py
+++ api/iheroes_api/config/environment.py
@@ -1,11 +1,18 @@
+from enum import Enum
 from typing import Callable
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import BaseSettings, HttpUrl, PostgresDsn
+
+
+class Env(str, Enum):
+    dev = "development"
+    testing = "testing"
+    prod = "production"
 
 
 class Settings(BaseSettings):
-    ENV: str
+    ENV: Env
     PYTHONPATH: str
     LOG_LEVEL: str
     DATABASE_PG_URL: PostgresDsn
@@ -13,6 +20,7 @@ class Settings(BaseSettings):
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
     JWT_ALGORITHM: str
     JWT_SECRET_KEY: str
+    LISTENER_URL: HttpUrl
     WEB_APP_DEBUG: bool
     WEB_APP_DESCRIPTION: str
     WEB_APP_TITLE: str
