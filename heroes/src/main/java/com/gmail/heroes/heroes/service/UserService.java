041b75d175b5f24b59a375918f9c123898fea01e
leandropinheiroo <leandropinheiro98@gmail.com>
2021-02-25 19:25:12 -0300

    Remove endpoint para buscar todos usuarios

--- heroes/src/main/java/com/gmail/heroes/heroes/service/UserService.java
+++ heroes/src/main/java/com/gmail/heroes/heroes/service/UserService.java
@@ -1,6 +1,5 @@
 package com.gmail.heroes.heroes.service;
 
-import com.gmail.heroes.heroes.domain.User;
 import com.gmail.heroes.heroes.repository.UserRepository;
 import com.gmail.heroes.heroes.service.mapper.UserMapper;
 import com.gmail.heroes.heroes.web.dto.UserDTO;
@@ -9,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,8 +22,4 @@ public class UserService {
         return this.userMapper.toDto(this.userRepository.save(this.userMapper.toEntity(userDTO)));
     }
 
-
-    public List<UserDTO> findAll(){
-        return this.userMapper.toDto(this.userRepository.findAll());
-    }
 }
