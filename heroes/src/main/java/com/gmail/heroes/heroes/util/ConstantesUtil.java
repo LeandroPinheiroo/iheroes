81d3bb4dd3d3643e6b6bbb37517b5e91cc1ed92b
leandropinheiroo <leandropinheiro98@gmail.com>
2021-02-27 09:48:40 -0300

    Criado mapeamento de ameacas

---
+++ heroes/src/main/java/com/gmail/heroes/heroes/util/ConstantesUtil.java
@@ -0,0 +1,10 @@
+package com.gmail.heroes.heroes.util;
+
+public class ConstantesUtil {
+
+    public static Integer radiusEarth = 6371;
+
+    public static Integer number_2 = 2;
+    public static Integer number_1 = 1;
+    public static Integer number_1000 = 1000;
+}
