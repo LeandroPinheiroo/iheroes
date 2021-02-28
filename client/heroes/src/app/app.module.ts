aa42233b799783ef89b4685115ed031970692fd7
leandropinheiroo <leandropinheiro98@gmail.com>
2021-02-28 14:17:30 -0300

    Conectando com socket IO ZRP

--- client/heroes/src/app/app.module.ts
+++ client/heroes/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SocketService } from './service/socket-service';
 import { HeroCreateComponent } from './views/hero/hero-create/hero-create.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -28,6 +29,8 @@ import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
+
+
 
 const APP_CONTAINERS = [
   DefaultLayoutComponent
@@ -103,7 +106,7 @@ export function tokenGetter() {
       provide: LocationStrategy,
       useClass: HashLocationStrategy
     },
-    IconSetService,
+    IconSetService
   ],
   bootstrap: [ AppComponent ]
 })
--- client/heroes/src/app/views/dashboard/dashboard.component.ts
+++ client/heroes/src/app/views/dashboard/dashboard.component.ts
@@ -1,13 +1,26 @@
+import { SocketService } from './../../service/socket-service';
 import { Component, OnInit } from '@angular/core';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 
 @Component({
-  templateUrl: 'dashboard.component.html'
+  templateUrl: 'dashboard.component.html',
+  styleUrls:['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  ngOnInit(){
-   
+  constructor(
+    private socketService: SocketService
+  ) {
+    
   }
+
+  ngOnInit(){
+    this.socketService.setConect();
+    this.socketService.listen().subscribe((data)=>{
+      console.log(data);
+    })
+  }
+
+
 }
